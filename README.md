6728186fa1279ad86ad329b5a31e358171b930e9
alietors <alietors@gmail.com>
2016-10-01 17:09:31 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# behave-test
+Testing how behave works
+
+# Pre-requisites
+
+Install behave
+
+```
+sudo pip install behave
+```
+
+Install selenium
+
+```
+sudo pip install behave
+```
+
+# Run
+```
+behave
+```
--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 
 def before_all(context):
-    context.browser = webdriver.Chrome('webdriver/chromedriver')
+    context.browser = webdriver.Chrome('features/webdriver/chromedriver')
 
 def after_all(context):
     context.browser.quit()
